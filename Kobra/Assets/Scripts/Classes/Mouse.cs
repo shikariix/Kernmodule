1a5883ca10442282ca73ad8b49cbd12f3d68ca23
shikariix <sarahsteenhuis@live.nl>
2017-10-04 20:56:09 +0200

    Kobra begin
    Added start of Kobra, my Snake based game.

---
+++ Kobra/Assets/Scripts/Classes/Mouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour, IDamagable {
+
+
+
+	void Start () {
+		//Generate a random position on start
+	}
+
+	//The mouse dies when hit with a bullet
+	public void Die() {
+		//Generate a new position on death
+		//Give player a point
+	}
+
+	Vector3 GeneratePos() {
+		//generate a random xPos and zPos value
+		//put it in a vector3
+		//return the vector into the transform.position of the mouse
+		//It cannot generate on a tile that is taken by the snake
+
+		Vector3 newPos = new Vector3 (0, 0, 0);
+
+		return newPos;
+	}
+}
---
+++ Kobra/Assets/Scripts/Classes/Snake.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour, IDamagable {
+
+	private int lenght;
+
+	//I need to identify the gameobject that I want to use as body
+	//The length decides how many bodyparts there should be
+	//The length can be increased and decreased
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//the snake dies when the head hits its body, a bullet hits it, or when it hits the mouse
+	public void Die() {
+		
+	}
+}
