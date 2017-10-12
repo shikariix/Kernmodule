af3fd02b98393a9c8e8e50a08466e3e965742503
shikariix <sarahsteenhuis@live.nl>
2017-10-12 15:48:30 +0200

    KOBRA v0.1
    Added first backup! Folder name is Korba_Attempt 2

---
+++ Kobra_Attempt 2/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	public static SceneChanger Instance { get; private set; }
+
+	//makes sure there is always one (1) SceneChanger in the scene
+	void Awake() {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GameMenu ();
+		}
+	}
+
+
+	public void PlayGame() {
+		SceneManager.LoadScene ("main");
+	}
+
+	public void GameMenu() {
+		SceneManager.LoadScene ("Menu");
+	}
+
+    public static void GameOver() {
+        SceneManager.LoadScene("GameOver");
+    }
+
+	public void ExitGame() {
+		Application.Quit ();
+		Debug.Log ("You cannot quit in the editor.");
+	}
+}
